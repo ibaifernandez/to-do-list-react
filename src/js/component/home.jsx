e558e9281d527eaebfe238c749dcba67b9a3c11c
Ibai Fernández <info@ibaifernandez.com>
2023-02-05 09:48:44 -0500

    Wrapping up practice

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,59 +1,62 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+    const [name, setName] = useState("");
+    const [tasks, setTasks] = useState([]);
 
-	const [name, setName] = useState('');
-	const [tasks, setTasks] = useState([]);
+    let nextTask = tasks.length;
 
-	let nextTask = tasks.length;
+    const addTask = (event) => {
+        if (event.key === "Enter") {
+            if (name.length !== 0) {
+                let tempTask = [...tasks, { id: nextTask, name: name }];
+                setTasks(tempTask);
+            }
+            setName("");
+        }
+    };
 
-	const addTask = (event) => {
-		if (event.key === 'Enter') {
-		  if ( name.length !== 0 ) {
-			let tempTask = [...tasks, { id:nextTask, name: name }];
-			setTasks(tempTask)
-		  }
-		  setName('');
-		}
-	}
+    const removeTask = (item) => {
+        let removeList = tasks.filter((task) => item.id !== task.id);
+        setTasks(removeList);
+    };
 
-	const removeTask = (item) => {
-		let removeList = tasks.filter((task) => item.id !== task.id);
-		setTasks(removeList);
-	}
+    return (
+        <div className="container-fluid text-center mt-5">
+            <h1 className="h1">To-Do List</h1>
+            <div className="main">
+                <input
+                    className="border-0 rounded-2"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Add a task..."
+                    onKeyDown={addTask}
+                />
 
-
-	return (
-		<div className="container text-center">
-			<h1>ToDos</h1>
-			<div className="main">
-				<ul className="list">
-					<li className="box border-0 text-muted fs-4 container-fluid rounded-2 p-2">
-						<input 
-							className="border-0 rounded-2"
-							value={name}
-							onChange={e => setName(e.target.value) }
-							placeholder="What needs to be done?"
-							onKeyDown={addTask}
-						/>
-					</li>
-					{tasks.map((task) => (
-						<li key={task.id} className="task mx-auto d-flex justify-content-between border-0 text-muted fs-4 container-fluid rounded-2 mt-1 mb-1">{task.name}
-						<button className="delete rounded-2 border-1 mt-1 mb-1" onClick={() => {removeTask(task)}}>
-							X
-						</button>
-						</li>
-					))}
-					<li className="items mx-auto rounded-2" >{nextTask} Items left</li>
-				</ul>
-			</div>
-		</div>
-	);
+                <ul>
+                    {tasks.map((task) => (
+                        <li
+                            key={task.id}
+                            className="w-50 d-flex justify-content-between align-items-center container-fluid rounded-2 my-3"
+                        >
+                            {task.name}
+                            <button
+                                className="delete rounded-2 border-1 my-2 p-1"
+                                onClick={() => {
+                                    removeTask(task);
+                                }}
+                            >
+                                ❌
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+                <div className="items mx-auto rounded-2">
+                    ~ {nextTask} items left to do ~
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
